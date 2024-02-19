cad3ac35d82b00cc49d6580cf5cae8ec44241948
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2024-02-20 03:18:34 +0530

    version bump

--- xleap/metrics/scores/__init__.py
+++ xleap/metrics/scores/__init__.py
@@ -1,7 +1,7 @@
 from .nlp import BLEU, Meteor, Rouge  # noqa
 from .sentiment import PromptSentiment, ResponseSentiment  # noqa
 from .stat import text_stat_metrics  # noqa
-from .toxicity import PromptToxicity, ResponseToxicity  # noqa
+# from .toxicity import PromptToxicity, ResponseToxicity  # noqa
 
 # llm based
 from .context_relevancy import ContextRelevancy  # noqa
